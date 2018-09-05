21d49a527210e68c6bf6e43969240f25398cedd8
kwitochka <kwitochka@hotmail.co.jp>
2018-09-05 18:06:03 +0300

    adds priority queue function

--- queue.js
+++ queue.js
@@ -38,3 +38,61 @@ console.log(q.front());
 q.print();
 console.log(q.size());
 console.log(q.isEmpty());
+
+function PriorityQueue() {
+  let collection = [];
+
+  this.printCollection = function() {
+    console.log(collection);
+  };
+
+  this.enqueue = function(el) {
+    if (this.isEmpty()) {
+      collection.push(el);
+    } else {
+      var added = false;
+      //if element's priority is less then array's element's
+      //put the element on the place of the array's element
+      for (let i = 0; i < collection.length; i++) {
+        if (el[1] < collection[i][1]) {
+          collection.splice(i, 0, el);
+          added = true;
+          break;
+        }
+      }
+      //else
+      if (!added) {
+        collection.push(el);
+      }
+    }
+  };
+
+  this.dequeue = function() {
+    const val = collection.shift();
+    return val[0];
+  };
+
+  this.front = function() {
+    return collection[0];
+  };
+
+  this.size = function() {
+    return collection.length;
+  };
+
+  this.isEmpty = function() {
+    return collection.length === 0;
+  };
+}
+
+const books = new PriorityQueue();
+books.enqueue(["Harry Potter", 5]);
+books.enqueue(["Atlant Shrugged", 1]);
+books.enqueue(["Bible", 1]);
+books.enqueue(["Sherlock Holmes", 3]);
+// books.printCollection();
+// console.log(books.front());
+books.dequeue();
+books.dequeue();
+books.printCollection();
+console.log(books.size());
