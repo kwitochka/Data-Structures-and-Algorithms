6aa0c2810ab0e7a2b9635532938f4afd5d412dc7
kwitochka <kwitochka@hotmail.co.jp>
2018-10-18 11:48:55 +0300

    Moved files to a new folder: Data structures

---
+++ calendar.js
@@ -0,0 +1,27 @@
+function holiday(x, weekDay, month, yearNumber) {
+  //coding and coding..
+  let Months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+  let Days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ];
+  let MonthsLengths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+}
