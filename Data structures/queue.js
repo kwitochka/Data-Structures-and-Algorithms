3adc3d98d63e8b813e8edc6ea3d8059721ca7a4b
kwitochka <kwitochka@hotmail.co.jp>
2018-10-30 15:42:28 +0200

    added linked list solution

--- Data structures/queue.js
+++ Data structures/queue.js
@@ -1,5 +1,67 @@
 //First In, first Out - a way to store data
 
+//Queue with linked list
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class linkedQueue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+  //add to the tail
+  //  'first' => 'second' => 'last'
+  enqueue(val) {
+    let node = new Node(val);
+
+    if (this.length === 0) {
+      this.first = node;
+      this.last = this.first;
+    } else {
+      this.last.next = node;
+      this.last = node;
+    }
+    return ++this.length;
+  }
+  //remove from the head
+  dequeue() {
+    //if length === o return null
+    let removedVal = this.first;
+    if (this.length === 0) return null;
+    //if length === 1 set all to null
+    if (this.length === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+    }
+    this.length--;
+    return removedVal;
+    //else remove this.first
+  }
+}
+
+//examples
+
+let qu = new linkedQueue();
+qu.enqueue("first");
+qu.enqueue("second");
+qu.enqueue("third");
+console.log(qu);
+qu.dequeue();
+console.log(qu);
+qu.dequeue();
+console.log(qu);
+qu.dequeue();
+console.log(qu);
+
+//factory and arrays
 function Queue() {
   let collection = [];
 
@@ -32,12 +94,12 @@ let q = new Queue();
 q.enqueue("Books");
 q.enqueue("Plates");
 q.enqueue("CDs");
-q.print();
+// q.print();
 q.dequeue();
-console.log(q.front());
-q.print();
-console.log(q.size());
-console.log(q.isEmpty());
+// console.log(q.front());
+// q.print();
+// console.log(q.size());
+// console.log(q.isEmpty());
 
 function PriorityQueue() {
   let collection = [];
@@ -94,5 +156,5 @@ books.enqueue(["Sherlock Holmes", 3]);
 // console.log(books.front());
 books.dequeue();
 books.dequeue();
-books.printCollection();
-console.log(books.size());
+// books.printCollection();
+// console.log(books.size());
